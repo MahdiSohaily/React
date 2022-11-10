9e9f8e623830b6f912014e8b71701f8153780213
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-10 21:03:42 +0430

    Add script to display post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
-export default function Post() {
+export default function Post({ data }) {
   return (
     <div className="post">
-      <img src="./assets/1.jpg" alt="" />
-      <h1>Post Title</h1>
-      <div>
-        She never liked cleaning the sink. It was beyond her comprehension how
-        it got so dirty so quickly. It seemed that she was forced to clean it
-        every other day. Even when she was extra careful to keep things clean
-        and orderly, it still ended up looking like a mess in a couple of days.
-        What she didn't know was there was a tiny creature living in it that
-        didn't like things neat.
-      </div>
+      <img src={`./assets/${data.image}`} alt="" />
+      <h1>{data.title}</h1>
+      <div>{data.body}</div>
     </div>
   );
 }
--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Comments from './Comments';
 import Post from './Post';
 
-export default function PostContainer({data}) {
+export default function PostContainer({ data }) {
   return (
     <div className="container">
-      <Post />
+      <Post data={data} />
       <Comments />
     </div>
   );
