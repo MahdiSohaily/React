ee6a041be354a542ac3e3f9df2a39539f9714ac0
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-12 08:08:00 +0430

    Removed old project files

--- src/Services/getData.js
+++
@@ -1,6 +0,0 @@
-async function getPosts() {
-  const posts = await fetch('  http://localhost:3001/posts');
-  return posts.json();
-}
-
-export default getPosts;
--- src/components/Link.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Link({ id, title, handleNavigation }) {
-  return <li onClick={() => handleNavigation(id)}>{title}</li>;
-}
