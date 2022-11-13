13bd06bbaa93c45e48d4b520db74a98f1148dd63
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-13 08:54:07 +0430

    Removed unwanted files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Blog from './components/Blog';
 import './index.css';;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Blog />
   </React.StrictMode>
 );
 
