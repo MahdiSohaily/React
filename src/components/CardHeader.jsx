0f043a0406f6591f835bea1ac94bb2b4ea1f5080
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-13 20:59:46 +0430

    Removed Unwanted files

--- src/components/CardHeader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function CardHeader({ img, name }) {
-  return (
-    <div className={`clash-card__image clash-card__image--${name}`}>
-      <img src={img} alt={name} />
-    </div>
-  );
-}
