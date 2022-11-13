41b28e34a3548ea4b3e59d694a37e9e7be5bf524
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-13 08:56:08 +0430

    Add project styles

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,4 +3,8 @@ import React, { useState, useEffect } from 'react';
 export default function Slider() {
   const [card, setCard] = useState([]);
   const [current, setCurrent] = useState(0);
+
+  return(
+    
+  )
 }
