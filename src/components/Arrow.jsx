f526176313415bf2705bf0905aff040a85fa8cf8
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-13 09:19:56 +0430

    Update project components content

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function Arrow() {
+export default function Arrow({ type }) {
   return (
     <div class="arrow-wrapper">
       <div class="round">
         <div id="cta">
-          <span class="arrow prev"></span>
+          <span class={`arrow ${type}`}></span>
         </div>
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import CardHeader from './CardHeader';
+import CardBody from './CardBody';
+import CardFooter from './CardFooter';
 
 export default function Card() {
   return (
-    <div>Card</div>
-  )
+    <div class="clash-card barbarian">
+      <CardHeader />
+      <CardBody />
+      <CardFooter />
+    </div>
+  );
 }
