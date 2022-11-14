d06402055f41e538747fa9c279d875e1fcbc524e
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-14 11:04:18 +0430

    Add script to check post ID validity

--- src/componets/Message.jsx
+++ src/componets/Message.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Toast from './Toast/';
 
 export default function Message() {
-    const [postID, setPostID] = useState(1)
+  const [postID, setPostID] = useState(1);
+  useEffect(() => {}, [postID]);
+  function handleChange(e) {
+    const { value } = e.target;
+    if (value >= 0) {
+      setPostID(value);
+      return true;
+    }
+    alert(`post ID can't be negative number `);
+  }
   return (
     <>
-      <input type="number" name="postID"  />
+      <label>
+        {' Post ID: '}
+        <input
+          type="number"
+          name="postID"
+          value={postID}
+          min={0}
+          onChange={handleChange}
+        />
+      </label>
     </>
   );
 }
