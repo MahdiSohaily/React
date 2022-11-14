03433c0f628f9be588f11345f16dda6d1d121e7c
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-14 11:26:32 +0430

    Display Post Title

--- src/componets/Message.jsx
+++ src/componets/Message.jsx
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import Loading from './Loading/Loading';
 import Toast from './Toast/';
 
 export default function Message() {
   const [postID, setPostID] = useState(1);
-  useEffect(() => {}, [postID]);
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setMessage('Post received Successfully' + Math.random());
+      setLoading(false);
+    }, 2000);
+  }, [postID]);
+
   function handleChange(e) {
     const { value } = e.target;
     if (value >= 0) {
       setPostID(value);
+      setLoading(true);
       return true;
     }
     alert(`post ID can't be negative number `);
   }
+
   return (
     <>
+      <h1>{`New post Displayed with ID ${Math.random()}`}</h1>
       <label>
         {' Post ID: '}
         <input
@@ -24,6 +37,8 @@ export default function Message() {
           onChange={handleChange}
         />
       </label>
+      {loading && <Loading />}
+      <Toast type="success" message={message} />
     </>
   );
 }
