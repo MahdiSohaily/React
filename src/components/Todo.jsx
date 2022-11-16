997467c25b502d0828e039f9f09a9f72e27b5148
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-16 09:50:45 +0430

    Add add new todo script

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import store from '../redux/store';
 import { ACTIONS } from '../redux/todos/todos';
 
 export default function Todo() {
-  const todos = store.getState().map((item) => {
+  const [, forcUpdate] = useState(0);
+  const data = store.getState();
+  const index = data.length + 1;
+  const todos = data.map((item) => {
     return (
-      <li key={item.id} className={item.completed ? 'completed' : ''}>
+      <li
+        key={item.id}
+        onClick={() => toggleTodo(item.id)}
+        className={item.completed ? 'completed' : ''}
+      >
         {item.text}
       </li>
     );
   });
+
+  useEffect(() => {
+    const unsbscribe = store.subscribe(() => forcUpdate((c) => c + 1));
+    return () => unsbscribe();
+  },);
+
+  function addTodo(e) {
+    if (e.code === 'Enter') {
+      store.dispatch({
+        type: ACTIONS.ADD_TODO,
+        payload: { id: index, text: e.target.value, completed: false },
+      });
+      e.target.value = '';
+    }
+  }
+
+  function toggleTodo(index) {
+    alert(index);
+    store.dispatch({
+      type: ACTIONS.TOGGLE_TODO,
+      payload: index,
+    });
+  }
+
   return (
     <>
-      {console.log(store.getState())}
+      {console.log(todos)}
       <label htmlFor="task">
         {'Task:'}
-        <input id="task" type="text" name="task" required />
+        <input id="task" type="text" name="task" required onKeyDown={addTodo} />
       </label>
       <ul>{todos}</ul>
     </>
